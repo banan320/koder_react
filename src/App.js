223e46a0d4ffb3e8018c89d82568eb28b76ee3cd
Alex Gerber <alex-gerber@mail.ru>
2023-02-20 16:56:47 +0300

    massive

--- src/App.js
+++ src/App.js
@@ -10,18 +10,43 @@ import { useState } from "react";
 
 // !============
 function App() {
-  const [value, setValue] = useState("1");
+  const [arr, setArr] = useState([1, 2, 3, 4, 5]);
+  const [value, setValue] = useState("");
 
-  function handleChange(event) {
-    setValue(event.target.value);
+  const result = arr.map((el, i) => (
+    <p key={i}>
+      {el}
+      <button onClick={() => remove(i)}>delete</button>
+      <button onClick={() => edit(i)}>edit</button>
+    </p>
+  ));
+
+  function add() {
+    setArr([...arr, value]);
   }
+
+  function edit(i) {
+    setArr([...arr.slice(0, i), "hello", ...arr.slice(i + 1)]);
+  }
+
+  function remove(i) {
+    setArr([...arr.slice(0, i), ...arr.slice(i + 1)]);
+  }
+  function inputChange(e) {
+    setValue(e.target.value);
+  }
+
+  function reverse() {
+    setArr([...arr.slice().reverse()]);
+  }
+
   return (
     <div className="App">
-      <input onChange={handleChange} defaultValue={value} />
-      урок 25
-      <p>text: {value}</p>
+      {result}
+      <input type="text" value={value} onChange={inputChange} />
+      <button onClick={add}>add element</button>
+      <button onClick={reverse}>reverse</button>
     </div>
   );
 }
-
 export default App;
