6445973e0386006ad2d78c2318b96845ad603bbd
Alex Gerber <alex-gerber@mail.ru>
2023-02-21 17:48:27 +0300

    lesson 29

--- src/App.js
+++ src/App.js
@@ -10,42 +10,33 @@ import { useState } from "react";
 
 // !============
 function App() {
-  const [arr, setArr] = useState([1, 2, 3, 4, 5]);
-  const [value, setValue] = useState("");
+  const [notes, setNotes] = useState([5252, 1, 2, 3, 4, 5]);
+  const [editNum, setEditNum] = useState(null);
 
-  const result = arr.map((el, i) => (
-    <p key={i}>
-      {el}
-      <button onClick={() => remove(i)}>delete</button>
-      <button onClick={() => edit(i)}>edit</button>
-    </p>
-  ));
+  const result = notes.map((note, index) => {
+    return (
+      <p key={index}>
+        {note}
+        <button onClick={() => setEditNum(index)}>edit</button>
+      </p>
+    );
+  });
 
-  function add() {
-    setArr([...arr, value]);
+  function changeItem(event) {
+    setNotes([
+      ...notes.slice(0, editNum),
+      event.target.value,
+      ...notes.slice(editNum + 1),
+    ]);
   }
-
-  function edit(i) {
-    setArr([...arr.slice(0, i), "hello", ...arr.slice(i + 1)]);
-  }
-
-  function remove(i) {
-    setArr([...arr.slice(0, i), ...arr.slice(i + 1)]);
-  }
-  function inputChange(e) {
-    setValue(e.target.value);
-  }
-
-  function reverse() {
-    setArr([...arr.slice().reverse()]);
-  }
-
   return (
     <div className="App">
       {result}
-      <input type="text" value={value} onChange={inputChange} />
-      <button onClick={add}>add element</button>
-      <button onClick={reverse}>reverse</button>
+      <input
+        value={editNum !== null ? notes[editNum] : ""}
+        onChange={changeItem}
+        onBlur={() => setEditNum(null)}
+      />
     </div>
   );
 }
