ab797e758c1b19f25e36c456eb57d471f23b991a
Alex Gerber <alex-gerber@mail.ru>
2023-03-16 16:05:30 +0300

    урок 51, решение задачи с температурой

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,32 +1,68 @@
-import { React, useState, useMemmo } from "react";
+import { React, useState } from "react";
 
-const scaleNames = [{ t: "Цельсия" }, { t: "Фаренгейта" }];
+// const scaleNames = [{ t: "Цельсия" }, { t: "Фаренгейта" }];
+const scaleNames = {
+  c: "Цельсия",
+  f: "Фаренгейта",
+};
 
-const TemperatureInput = ({ scale, setState, temp }) => {
+const TemperatureInput = ({ scale, onChange, temp }) => {
   return (
     <div>
-      <h2>Введите температуру в градусах {scale}:</h2>
-      <input value={temp} />
+      <h2>Введите температуру в градусах {scaleNames[scale]}:</h2>
+      <input value={temp} onChange={(e) => onChange(e.target.value)} />
     </div>
   );
 };
 
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
 const Calculator = () => {
-  const [fahr, setFahr] = useState(0);
-  const [cels, setCels] = useState(0);
-  const [scale, setScale] = useState(scaleNames);
+  const [temperature, setTemperature] = useState("");
+  const [scale, setScale] = useState("c");
+
+  const handleCelsiusChange = (temperature) => {
+    setTemperature(temperature);
+    setScale("c");
+  };
+
+  const handleFahrenheitChange = (temperature) => {
+    setTemperature(temperature);
+    setScale("f");
+  };
+
+  const celsius =
+    scale === "f" ? tryConvert(temperature, toCelsius) : temperature;
+  const fahrenheit =
+    scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature;
 
   return (
     <div>
       <TemperatureInput
-        scale={scale.c}
-        temp={cels}
-        setCels={(e) => setCels(e.target.value)}
+        scale="c"
+        temp={celsius}
+        onChange={handleCelsiusChange}
       />
       <TemperatureInput
-        scale={scale.f}
-        temp={fahr}
-        setCels={(e) => setFahr(e.target.value)}
+        scale="f"
+        temp={fahrenheit}
+        onChange={handleFahrenheitChange}
       />
     </div>
   );
